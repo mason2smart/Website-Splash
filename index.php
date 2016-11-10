6f18f444154a5a7c0d21dd8df4fb7ccd2db5eada
Mason <mason.s.cole@hotmail.com>
2016-11-10 15:48:12 -0600

    Mobile Version

--- index.php
+++ index.php
@@ -1,317 +1,354 @@
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
-  <head>
-       <meta http-equiv="X-UA-Compatible" content="IE=edge">
-           <meta name="description" content="Discobots - First Robotics Team 2587 Main Directory">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="material/material.min.css">
-<script src="material/material.min.js"></script>
-<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-       <meta name="mobile-web-app-capable" content="yes">
-<meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
-    <title>Discobots 2587 | Splash</title>
-    
-  </head>
-  <body class="mdl-color--blue-A700">
-             <center>
-        <!------------------------------------------>
-        <!------------------------------------------>
-    		<!-- Simple header with fixed tabs. -->
-<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header
-            mdl-layout--fixed-tabs">
-  <header class="mdl-layout__header">
-    <div class="mdl-layout__header-row mdl-shadow--20dp">
-      <!-- Title -->
-      <span class="mdl-layout-title">DISCOBOTS SPLASH</span>
-                      <div align="right">
 
-                        <section class="section--right mdl-grid mdl-grid--2dp" style="text-align: right">
-                        <li class="mdl-layout__tab mdl-button mdl-js-button mdl-js-ripple-effect">
-	            <a href="https://www.facebook.com/DiscoBots2587">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="Discobots - First Robotics Team 2587 Main Directory">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="material/material.min.css">
+	<script src="material/material.min.js"></script>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<meta name="mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<title>Discobots 2587 | Splash</title>
+
+</head>
+
+<body class="mdl-color--blue-A700">
+	<center>
+		<!------------------------------------------>
+		<!------------------------------------------>
+		<!-- Simple header with fixed tabs. -->
+		<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header
+            mdl-layout--fixed-tabs">
+			<header class="mdl-layout__header">
+				<div class="mdl-layout__header-row mdl-shadow--6dp">
+					<!-- Title -->
+					<span class="mdl-layout-title">DISCOBOTS SPLASH</span>
+
+					<div class="mdl-layout-spacer"></div>
+						<section class="mdl-grid mdl-cell--hide-phone 
+" style="text-align: right">
+						
+							<li class="mdl-layout__tab mdl-button mdl-js-button mdl-js-ripple-effect">
+								<a href="https://www.facebook.com/DiscoBots2587">
 	              <img src="static/facebook.svg" alt="Facebook" width="30" height="30" />
-	            </a>					
-	            </li>
-			<li class="mdl-layout__tab mdl-button mdl-js-button mdl-js-ripple-effect">
-	            <a href="https://twitter.com/discobots">
+	            </a>
+							
+							</li>
+							<li class="mdl-layout__tab mdl-button mdl-js-button mdl-js-ripple-effect">
+								<a href="https://twitter.com/discobots">
 	              <img src="static/twitter.svg" alt="Twitter" width="30" height="30" />
 	            </a>
-		 </li>
-            <li class="mdl-layout__tab mdl-button mdl-js-button mdl-js-ripple-effect">
-	            <a href="https://plus.google.com/+DiscoBots/">
+							
+							</li>
+							<li class="mdl-layout__tab mdl-button mdl-js-button mdl-js-ripple-effect">
+								<a href="https://plus.google.com/+DiscoBots/">
 	              <img src="static/google.svg" alt="Google+" width="30" height="30" />
 	            </a>
-		 </li>
-            <li class="mdl-layout__tab mdl-button mdl-js-button mdl-js-ripple-effect">
-	            <a href="https://github.com/discobots2587/">
+							
+							</li>
+							<li class="mdl-layout__tab mdl-button mdl-js-button mdl-js-ripple-effect">
+								<a href="https://github.com/discobots2587/">
 	              <img src="static/github.png" alt="GitHub" width="30" height="30" />
 	            </a>
-		 </li>
-	</section>
-       </div>
-	  </div>
-    
-    <!-- Tabs -->
-    <div class="mdl-layout__tab-bar mdl-color--primary-dark">
-               <section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
+							
+							</li>
+						</section>
+				</div>
+			  </header>
 
-      <a href="http://2015.discobots.org/" class="mdl-layout__tab mdl-button mdl-js-button mdl-js-ripple-effect ">2015</a>
-      <a href="http://2014.discobots.org/" class="mdl-layout__tab mdl-button mdl-js-button mdl-js-ripple-effect">2014</a>
-      <a href="http://2013.discobots.org/" class="mdl-layout__tab mdl-button mdl-js-button mdl-js-ripple-effect">2013</a>
-      <a href="http://2012.discobots.org/" class="mdl-layout__tab mdl-button mdl-js-button mdl-js-ripple-effect">2012</a>
-      <a href="http://2011.discobots.org/" class="mdl-layout__tab mdl-button mdl-js-button mdl-js-ripple-effect">2011</a>
-      <a href="http://2010.discobots.org/" class="mdl-layout__tab mdl-button mdl-js-button mdl-js-ripple-effect">2010</a>
-      <a href="http://2009.discobots.org/" class="mdl-layout__tab mdl-button mdl-js-button mdl-js-ripple-effect">2009</a>
-      <a href="http://2008.discobots.org/" class="mdl-layout__tab mdl-button mdl-js-button mdl-js-ripple-effect">2008</a>
-		</section>
-   </div>
-  </header>
-			  <!------------------------------------------>
-<main>
-     	 
-    <ul class='slideshow'>
-  <li>
-      	 <span></span>
 
-	  <h3>The Discobots<br/><font size ="5">FIRST Robotics Team 2587<br/>Carnegie Vanguard High School, Houston, Texas</font></h3>
-   	 
-  </li>
-  <li>
-    	   <span></span>
-  </li>
-  <li>
-    	  <span></span>
-  </li>
-  <li>
-    	  <span></span>
-  </li>
-    <li>
-    	  <span></span>
-  </li>
-     <li>
-    	  <span></span>
-  </li>
-</ul>
-    	 
-    	 <style>
-			 html {
-  min-height: 100%;
-}
+				<!-- Tabs -->
+				<div>
+				<div class="mdl-layout--large-screen-only mdl-layout__tab-bar mdl-color--primary-dark mdl-shadow--2dp mdl-js-ripple-effect" style="max-height: 50% z-index: 4;">
+					
+						
+						<a href="http://2013.discobots.org/" class="mdl-tabs__tab mdl-layout__tab mdl-button mdl-js-button mdl-js-ripple-effect mdl-color-text--grey ">2013</a>
+						<a href="http://2014.discobots.org/" class="mdl-tabs__tab mdl-layout__tab mdl-button mdl-js-button mdl-js-ripple-effect mdl-color-text--grey">2014</a>
+						<a href="http://2013.discobots.org/" class="mdl-tabs__tab mdl-layout__tab mdl-button mdl-js-button mdl-js-ripple-effect mdl-color-text--grey ">2013</a>
+						<a href="http://2012.discobots.org/" class="mdl-tabs__tab mdl-layout__tab mdl-button mdl-js-button mdl-js-ripple-effect mdl-color-text--grey ">2012</a>
+						<a href="http://2011.discobots.org/" class="mdl-tabs__tab mdl-layout__tab mdl-button mdl-js-button mdl-js-ripple-effect mdl-color-text--grey ">2011</a>
+						<a href="http://2010.discobots.org/" class="mdl-tabs__tab mdl-layout__tab mdl-button mdl-js-button mdl-js-ripple-effect mdl-color-text--grey ">2010</a>
+						<a href="http://2009.discobots.org/" class="mdl-tabs__tab mdl-layout__tab mdl-button mdl-js-button mdl-js-ripple-effect mdl-color-text--grey ">2009</a>
+						<a href="http://2008.discobots.org/" class="mdl-tabs__tab mdl-layout__tab mdl-button mdl-js-button mdl-js-ripple-effect mdl-color-text--grey ">2008</a>
+					</div>
+			</div>
+					
+					  <!--  <div class="mdl-layout__header-row">
+      <span class="mdl-layout-title">On This Site</span>
+      <div class="mdl-layout-spacer"></div>
+      <nav class="mdl-navigation">
+                <a class="mdl-navigation__link" href="#">Home</a>
+                <a class="mdl-navigation__link" href="#">About</a>
+                <a class="mdl-navigation__link" href="#">Contact</a>
+      </nav>
+    </div>-->
+    <div class="mdl-layout__drawer mdl-layout--small-screen-only">DiscoPages<nav class="mdl-navigation">
+      <a class="mdl-navigation__link" href="#">Products</a>
+      <a class="mdl-navigation__link" href="#">Services</a>
+      <a class="mdl-navigation__link" href="#">Portfolios</a>
+      <a class="mdl-navigation__link" href="#">Achievements</a>
+      <a class="mdl-navigation__link" href="#">Blog</a>
+    </nav>
+  </div>			<!------------------------------------------>
+			<main class="mdl-layout__content">
 
-body {
-  height: 100%;
-}
+				<ul class='slideshow'>
+					<li>
+						<span></span>
 
-.slideshow {
-  list-style: none;
-  z-index: 1;
-}
-.slideshow li span {
-  width: 100%;
-	height: 100%;
-  position: absolute;
-  top: 104px;
-  left: 0px;
-  color: transparent;
-  background-size: cover;
-  background-position: 50% 50%;
-  background-repeat: none;
-  opacity: 0;
-  z-index: 0;
-  -webkit-backface-visibility: hidden;
-  backface-visibility: hidden;
-  -webkit-animation: imageAnimation 32s linear infinite 0s;
-  -moz-animation: imageAnimation 32s linear infinite 0s;
-  animation: imageAnimation 32s linear infinite 0s;
-}
-.slideshow li h3 {
-  position: absolute;
-  text-align: center;
-  z-index: 2;
-  bottom: 150px;
-  left: 0;
-  right: 0;
-  opacity: 0;
-  font-size: 4.0625em;
-  font-family: 'Josefin Sans', sans-serif;
-  text-transform: uppercase;
-  color: #fff;
-  -webkit-animation: titleAnimation 32s linear 1 0s;
-  -moz-animation: titleAnimation 32s linear 1 0s;
-  animation: titleAnimation 32s linear 1 0s;
-}
-@media only screen and (min-width: 768px) {
-  .slideshow li h3 {
-    bottom: 30px;
-    font-size: 8.125em;
-  }
-}
-@media only screen and (min-width: 1024px) {
-  .slideshow li h3 {
-    font-size: 10.9375em;
-  }
-}
+						<h3>The Discobots<br/><font size ="5">FIRST Robotics Team 2587<br/>Carnegie Vanguard High School, Houston, Texas</font></h3>
 
-.slideshow li:nth-child(1) span {
-  background-image: url(static/image4-1.png);
-}
+					</li>
+					<li>
+						<span></span>
+					</li>
+					<li>
+						<span></span>
+					</li>
+					<li>
+						<span></span>
+					</li>
+					<li>
+						<span></span>
+					</li>
+					<li>
+						<span></span>
+					</li>
+				</ul>
 
-.slideshow li:nth-child(2) span {
-  background-image: url(static/image1.jpg);
-  -webkit-animation-delay: 6s;
-  -moz-animation-delay: 6s;
-  animation-delay: 6s;
-}
+				<style>
+					html {
+						min-height: 100%;
+					}
+					
+					body {
+						height: 100%;
+					}
+					
+					.slideshow {
+						list-style: none;
+						z-index: 1;
+					}
+					
+					.slideshow li span {
+						width: 100%;
+						height: 100%;
+						position: absolute;
+						top: 0px;
+						left: 0px;
+						color: transparent;
+						background-size: cover;
+						background-position: 50% 50%;
+						background-repeat: none;
+						opacity: 0;
+						z-index: 0;
+						-webkit-backface-visibility: hidden;
+						backface-visibility: hidden;
+						-webkit-animation: imageAnimation 32s linear infinite 0s;
+						-moz-animation: imageAnimation 32s linear infinite 0s;
+						animation: imageAnimation 32s linear infinite 0s;
+					}
+					
+					.slideshow li h3 {
+						position: absolute;
+						text-align: center;
+						z-index: 2;
+						bottom: 150px;
+						left: 0;
+						right: 0;
+						opacity: 0;
+						font-size: 4.0625em;
+						font-family: 'Josefin Sans', sans-serif;
+						text-transform: uppercase;
+						color: #fff;
+						-webkit-animation: titleAnimation 32s linear 1 0s;
+						-moz-animation: titleAnimation 32s linear 1 0s;
+						animation: titleAnimation 32s linear 1 0s;
+					}
+					
+					@media only screen and (min-width: 768px) {
+						.slideshow li h3 {
+							bottom: 30px;
+							font-size: 8.125em;
+						}
+					}
+					
+					@media only screen and (min-width: 1024px) {
+						.slideshow li h3 {
+							font-size: 10.9375em;
+						}
+					}
+					
+					.slideshow li:nth-child(1) span {
+						background-image: url(static/image4-1.png);
+					}
+					
+					.slideshow li:nth-child(2) span {
+						background-image: url(static/image1.jpg);
+						-webkit-animation-delay: 6s;
+						-moz-animation-delay: 6s;
+						animation-delay: 6s;
+					}
+					
+					.slideshow li:nth-child(3) span {
+						background-image: url(static/image3.jpg);
+						-webkit-animation-delay: 12s;
+						-moz-animation-delay: 12s;
+						animation-delay: 12s;
+					}
+					
+					.slideshow li:nth-child(4) span {
+						background-image: url(static/image2.jpg);
+						-webkit-animation-delay: 18s;
+						-moz-animation-delay: 18s;
+						animation-delay: 18s;
+					}
+					
+					.slideshow li:nth-child(5) span {
+						background-image: url(static/image5.JPG);
+						-webkit-animation-delay: 24s;
+						-moz-animation-delay: 24s;
+						animation-delay: 24s;
+					}
+					
+					.slideshow li:nth-child(6) span {
+						background-image: url(static/image6.jpg);
+						-webkit-animation-delay: 32s;
+						-moz-animation-delay: 32s;
+						animation-delay: 32s;
+					}
+					
+					@-webkit-keyframes imageAnimation {
+						0% {
+							opacity: 0;
+							-webkit-animation-timing-function: ease-in;
+						}
+						12.5% {
+							opacity: 1;
+							-webkit-animation-timing-function: ease-out;
+						}
+						25% {
+							opacity: 1;
+						}
+						37.5% {
+							opacity: 0;
+						}
+						100% {
+							opacity: 0;
+						}
+					}
+					
+					@-moz-keyframes imageAnimation {
+						0% {
+							opacity: 0;
+							-moz-animation-timing-function: ease-in;
+						}
+						12.5% {
+							opacity: 1;
+							-moz-animation-timing-function: ease-out;
+						}
+						25% {
+							opacity: 1;
+						}
+						37.5% {
+							opacity: 0;
+						}
+						100% {
+							opacity: 0;
+						}
+					}
+					
+					@keyframes imageAnimation {
+						0% {
+							opacity: 0;
+							-webkit-animation-timing-function: ease-in;
+							-moz-animation-timing-function: ease-in;
+							animation-timing-function: ease-in;
+						}
+						12.5% {
+							opacity: 1;
+							-webkit-animation-timing-function: ease-out;
+							-moz-animation-timing-function: ease-out;
+							animation-timing-function: ease-out;
+						}
+						25% {
+							opacity: 1;
+						}
+						37.5% {
+							opacity: 0;
+						}
+						100% {
+							opacity: 0;
+						}
+					}
+					
+					@-webkit-keyframes titleAnimation {
+						0% {
+							opacity: 0;
+						}
+						12.5% {
+							opacity: 1;
+						}
+						25% {
+							opacity: 1;
+						}
+						37.5% {
+							opacity: 0;
+						}
+						100% {
+							opacity: 0;
+						}
+					}
+					
+					@-moz-keyframes titleAnimation {
+						0% {
+							opacity: 0;
+						}
+						12.5% {
+							opacity: 1;
+						}
+						25% {
+							opacity: 1;
+						}
+						37.5% {
+							opacity: 0;
+						}
+						100% {
+							opacity: 0;
+						}
+					}
+					
+					@keyframes titleAnimation {
+						0% {
+							opacity: 0;
+						}
+						12.5% {
+							opacity: 1;
+						}
+						25% {
+							opacity: 1;
+						}
+						37.5% {
+							opacity: 0;
+						}
+						100% {
+							opacity: 0;
+						}
+					}
+					
+					.no-cssanimations .slideshow li span {
+						opacity: 1;
+					}
+				</style>
 
-.slideshow li:nth-child(3) span {
-  background-image: url(static/image3.jpg);
-  -webkit-animation-delay: 12s;
-  -moz-animation-delay: 12s;
-  animation-delay: 12s;
-}
+			</main>
 
-.slideshow li:nth-child(4) span {
-  background-image: url(static/image2.jpg);
-  -webkit-animation-delay: 18s;
-  -moz-animation-delay: 18s;
-  animation-delay: 18s;
-}
-			 
-.slideshow li:nth-child(5) span {
-  background-image: url(static/image5.JPG);
-  -webkit-animation-delay: 24s;
-  -moz-animation-delay: 24s;
-  animation-delay: 24s;
-}
-.slideshow li:nth-child(6) span {
-  background-image: url(static/image6.jpg);
-  -webkit-animation-delay: 32s;
-  -moz-animation-delay: 32s;
-  animation-delay: 32s;
-}
+		</div>
+		</div>
 
-@-webkit-keyframes imageAnimation {
-  0% {
-    opacity: 0;
-    -webkit-animation-timing-function: ease-in;
-  }
-  12.5% {
-    opacity: 1;
-    -webkit-animation-timing-function: ease-out;
-  }
-  25% {
-    opacity: 1;
-  }
-  37.5% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 0;
-  }
-}
-@-moz-keyframes imageAnimation {
-  0% {
-    opacity: 0;
-    -moz-animation-timing-function: ease-in;
-  }
-  12.5% {
-    opacity: 1;
-    -moz-animation-timing-function: ease-out;
-  }
-  25% {
-    opacity: 1;
-  }
-  37.5% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 0;
-  }
-}
-@keyframes imageAnimation {
-  0% {
-    opacity: 0;
-    -webkit-animation-timing-function: ease-in;
-    -moz-animation-timing-function: ease-in;
-    animation-timing-function: ease-in;
-  }
-  12.5% {
-    opacity: 1;
-    -webkit-animation-timing-function: ease-out;
-    -moz-animation-timing-function: ease-out;
-    animation-timing-function: ease-out;
-  }
-  25% {
-    opacity: 1;
-  }
-  37.5% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 0;
-  }
-}
-@-webkit-keyframes titleAnimation {
-  0% {
-    opacity: 0;
-  }
-  12.5% {
-    opacity: 1;
-  }
-  25% {
-    opacity: 1;
-  }
-  37.5% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 0;
-  }
-}
-@-moz-keyframes titleAnimation {
-  0% {
-    opacity: 0;
-  }
-  12.5% {
-    opacity: 1;
-  }
-  25% {
-    opacity: 1;
-  }
-  37.5% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 0;
-  }
-}
-@keyframes titleAnimation {
-  0% {
-    opacity: 0;
-  }
-  12.5% {
-    opacity: 1;
-  }
-  25% {
-    opacity: 1;
-  }
-  37.5% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 0;
-  }
-}
-.no-cssanimations .slideshow li span {
-  opacity: 1;
-}
+</body>
 
-	</style>
-     	 
-	</main>
-     
-    </div>
-    </div>
-
-  </body>
 </html>
