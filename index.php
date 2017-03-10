86be1d36a9937ff751e2f37a72e5731a2beef899
Mason <mason.s.cole@hotmail.com>
2017-03-10 17:46:08 -0600

    Revert "Switching to Swiper"
    This reverts commit 29527fc1b367dd34eaa705fa6308d04cc1b50d7d.

--- index.php
+++ index.php
@@ -7,16 +7,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="material/material.min.css">
 	<script src="material/material.min.js"></script>
-	    <link rel="stylesheet" href="dist/css/swiper.min.css">
-
-	<!--<script src="jquery_lazyload/jquery.lazyload.js" type="text/javascript"></script>-->
-
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 	<meta name="mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-status-bar-style" content="black">
 	<title>Discobots 2587 | Splash</title>
-
 
 </head>
 
@@ -99,26 +94,8 @@
     </nav>
   </div>			<!------------------------------------------>
 		  <main class="mdl-layout__content">
-<div class="swiper-container">
-    <!-- Additional required wrapper -->
-    <div class="swiper-wrapper">
-        <!-- Slides -->
-        <div class="swiper-slide">Slide 1</div>
-        <div class="swiper-slide">Slide 2</div>
-        <div class="swiper-slide">Slide 3</div>
-        ...
-    </div>
-    <!-- If we need pagination -->
-    <div class="swiper-pagination"></div>
-    
-    <!-- If we need navigation buttons -->
-    <div class="swiper-button-prev"></div>
-    <div class="swiper-button-next"></div>
-    
-    <!-- If we need scrollbar -->
-    <div class="swiper-scrollbar"></div>
-</div>
-				<!--<ul class='slideshow'>
+
+				<ul class='slideshow'>
 					<li>
 						<span></span>
 
@@ -365,30 +342,13 @@
 					.no-cssanimations .slideshow li span {
 						opacity: 1;
 					}
-				</style>-->
+				</style>
 
 			</main>
 			<font size="1"><p style="margin-top:0; margin-bottom: 0;">© Mason Cole</p></font>
 		</div>
 		</div>
-    <script src="dist/js/swiper.min.js"></script>
-  <script>        
-  var mySwiper = new Swiper ('.swiper-container', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-    
-    // If we need pagination
-    pagination: '.swiper-pagination',
-    
-    // Navigation arrows
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    
-    // And if we need scrollbar
-    scrollbar: '.swiper-scrollbar',
-  })        
-  </script>
+
 </body>
 
 </html>
